f974a294d97ca815bfc74beeae01f0d67ede71cb
Gaurav Kumar <gaurav31120@gmail.com>
2025-01-03 01:33:02 +0530

    3 Variables

---
+++ Basics/Variables.java
@@ -0,0 +1,12 @@
+class Variables{
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);   // 8
+    }
+
+}
+
+// Output
+// 8
