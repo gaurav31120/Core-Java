1b7a2bcc4bd540ea653281dd9c09e8b504e895de
Gaurav Kumar <gaurav31120@gmail.com>
2025-03-17 10:48:22 +0530

    11 ElseIfConditionals

---
+++ Conditional/ElseIfConditionals.java
@@ -0,0 +1,14 @@
+class ElseIfConditionals {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x > 10) {
+            System.out.println("Greater than 10");
+        } else if (x == 10) {
+            System.out.println("Equal to 10");
+        } else {
+            System.out.println("Smaller than 10");
+
+        }
+    }
+}
