d76a32a72138ae31ad652b86e85de9582f4fbb00
Gaurav Kumar <gaurav31120@gmail.com>
2025-03-17 10:17:47 +0530

    10 IfElseConditionals

---
+++ Conditional/IfElseConditionals.java
@@ -0,0 +1,12 @@
+class IfElseConditionals {
+    public static void main(String[] args) {
+        int x = 18;
+
+        if (x > 10) {
+            System.out.println("Greater than 10");
+        } else {
+            System.out.println("Smaller than 10");
+
+        }
+    }
+}
