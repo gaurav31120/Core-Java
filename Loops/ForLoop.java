f44cdb849e33bf0f45b07ab5a32fe433f50e5697
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-07 05:24:16 +0530

    17 For loop

---
+++ Loops/ForLoop.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=4;i++) {
+            System.out.println("Hii " + i);
+        }
+    }
+
+    // output:
+    // Hii 0
+    // Hii 1
+    // Hii 2
+    // Hii 3
+    // Hii 4
+    
+}
