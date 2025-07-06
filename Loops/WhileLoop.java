16dd2376dd8c07f2bebf24a23074172de132fcc3
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-07 05:17:07 +0530

    15 While loop

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {            
+        int i=1;
+        
+        while(i<=4) {
+            System.out.println("Hii " + i);
+            i++;
+        }
+    }
+    
+}
