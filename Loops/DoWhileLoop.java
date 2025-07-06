abf8a2a6139dd2b33b1931288f47af36065ace71
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-07 05:20:48 +0530

    16 Do While loop

---
+++ Loops/DoWhileLoop.java
@@ -0,0 +1,14 @@
+ package Loops;
+
+class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Hii " + i); 
+            i++;
+        }
+        while(i<=4);
+    }
+}
